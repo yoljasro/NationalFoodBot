056b1f55096622a563e272242d52160ee701359d
yoljasro <saidaliyevjasur450@gmail.com>
2024-09-19 16:06:28 +0500

    changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 const token = '6837472952:AAE_uj8Ovl5ult8urjEVQUWptSKSJKBzws4';
-const webAppUrl = 'https://milliyfront-last.vercel.app/';
+const webAppUrl = 'https://milliyfront-ju7q.vercel.app';
 
 // Botni yaratamiz
 const bot = new TelegramBot(token, { polling: true });
